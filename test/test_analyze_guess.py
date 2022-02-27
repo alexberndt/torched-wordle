3350292e9db4bb9fdc86ddd5daff6557f02f5ea1
Alex Berndt <berndtae@gmail.com>
2022-02-27 22:53:51 +0100

    Updated unittests

--- test/test_analyze_guess.py
+++ test/test_analyze_guess.py
@@ -3,16 +3,12 @@ from wordle_torch.response import Response as r
 
 
 def test_basic():
-
     game = WordleGame("spilt")
-    response = game.guess("spoil")
-    expected = [r.GREEN, r.GREEN, r.GREY, r.YELLOW, r.YELLOW]
-    assert expected == response
+    assert game.guess("spoil") == [r.GREEN, r.GREEN, r.GREY, r.YELLOW, r.YELLOW]
+
 
 def test_answer_abbey_1():
-
     game = WordleGame("abbey")
-
     assert game.guess("algae") == [r.GREEN, r.GREY, r.GREY, r.GREY, r.YELLOW]
     assert game.guess("keeps") == [r.GREY, r.YELLOW, r.GREY, r.GREY, r.GREY]
     assert game.guess("orbit") == [r.GREY, r.GREY, r.GREEN, r.GREY, r.GREY]
@@ -21,9 +17,7 @@ def test_answer_abbey_1():
       
 
 def test_answer_abbey_2():
-
     game = WordleGame("abbey")
-
     assert game.guess("opens") == [r.GREY, r.GREY, r.YELLOW, r.GREY, r.GREY]
     assert game.guess("babes") == [r.YELLOW, r.YELLOW, r.GREEN, r.GREEN, r.GREY]
     assert game.guess("kebab") == [r.GREY, r.YELLOW, r.GREEN, r.YELLOW, r.YELLOW]
@@ -32,9 +26,7 @@ def test_answer_abbey_2():
 
 
 def test_answer_shire():
-
     game = WordleGame("shire")
-
     assert game.guess("adieu") == [r.GREY, r.GREY, r.GREEN, r.YELLOW, r.GREY]
     assert game.guess("ships") == [r.GREEN, r.GREEN, r.GREEN, r.GREY, r.GREY]
     assert game.guess("shive") == [r.GREEN, r.GREEN, r.GREEN, r.GREY, r.GREEN]
@@ -44,9 +36,7 @@ def test_answer_shire():
 
 
 def test_answer_elder():
-
     game = WordleGame("elder")
-
     assert game.guess("naval") == [r.GREY, r.GREY, r.GREY, r.GREY, r.YELLOW]
     assert game.guess("evade") == [r.GREEN, r.GREY, r.GREY, r.YELLOW, r.YELLOW]
     assert game.guess("fluff") == [r.GREY, r.GREEN, r.GREY, r.GREY, r.GREY]
@@ -55,8 +45,15 @@ def test_answer_elder():
     assert game.guess("paddy") == [r.GREY, r.GREY, r.GREEN, r.GREY, r.GREY]
     assert game.guess("elder") == [r.GREEN, r.GREEN, r.GREEN, r.GREEN, r.GREEN]
 
+
 def test_answer_fluff():
-
     game = WordleGame("fluff")
-
     assert game.guess("naval") == [r.GREY, r.GREY, r.GREY, r.GREY, r.YELLOW]
+
+
+def test_answer_crimp():
+    game = WordleGame("crimp")
+    assert game.guess("raise") == [r.YELLOW, r.GREY, r.GREEN, r.GREY, r.GREY]
+    assert game.guess("mount") == [r.YELLOW, r.GREY, r.GREY, r.GREY, r.GREY]
+    assert game.guess("grime") == [r.GREY, r.GREEN, r.GREEN, r.GREEN, r.GREY]
+    assert game.guess("crimp") == [r.GREEN, r.GREEN, r.GREEN, r.GREEN, r.GREEN]
